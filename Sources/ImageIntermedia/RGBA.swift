1ebfedfa0834e2c519eacf167aeda174baf6f5bd
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-08-24 15:21:32 +0800

    Add conversion functions among 8, 10, 12 and 16-bit

--- Sources/ImageIntermedia/RGBA.swift
+++ Sources/ImageIntermedia/RGBA.swift
@@ -8,7 +8,7 @@
 /// In the RGBA byte order, the colors are stored in memory such that R is at
 /// the lowest address, G comes after it, B follows, and A is last.
 ///
-/// RGBA12121212 layout: [UInt16]
+/// RGBA12x4 layout: [UInt16]
 ///
 ///     low memory address    ---->      high memory address
 ///     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1  (bits)
@@ -22,7 +22,7 @@
 ///
 /// Remapping is needed if it comes from 8-bit or 10-bit RGBA colors.
 ///
-/// RGBA10101010 layout: [UInt16]
+/// RGBA10x4 layout: [UInt16]
 ///
 ///     low memory address    ---->      high memory address
 ///     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1  (bits)
@@ -34,31 +34,96 @@
 ///   ↖                              .....
 ///    Array Index
 ///
-/// RGBA8888 layout: [UInt8]
+/// RGBA8x4 layout: [UInt16]
 ///
 ///     low memory address    ---->      high memory address
 ///     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1  (bits)
 ///  0 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-///    |   Red         |   Green       |   Blue        |   Alpha       |
+///    |     Red       |    ZeroPad    |     Green     |    ZeroPad    |
+///  2 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+///    |     Blue      |    ZeroPad    |     Alpha     |    ZeroPad    |
 ///  4 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+///   ↖                              .....
+///    Array Index
 
-public func rgba_10to12(_ rgba10101010 : [UInt16]) -> [UInt16] {
-    /* Buggy */
-    return rgba10101010.map { $0 << 2 | $0 >> 2 }
+//----------------------------------------------------------------------------//
+//                             8-bit source                                   //
+//----------------------------------------------------------------------------//
+
+@inlinable
+public func rgba_8to10(_ rgba8x4 : [UInt16]) -> [UInt16] {
+    return rgba8x4.map { $0 << 2 | $0 >> 6 }
 }
 
-public func rgba_8to10(_ rgba8888 : [UInt8]) -> [UInt16] {
-    return rgba8888.map { (UInt16($0) << 2 | UInt16($0) >> 6) }
+@inlinable
+public func rgba_8to12(_ rgba8x4 : [UInt16]) -> [UInt16] {
+    return rgba8x4.map { $0 << 4 | $0 >> 4 }
 }
 
-public func rgba_8to12(_ rgba8888 : [UInt8]) -> [UInt16] {
-    return rgba8888.map { UInt16($0) << 4 | UInt16($0) >> 4 }
+@inlinable
+public func rgba_8to16(_ rgba8x4 : [UInt16]) -> [UInt16] {
+    return rgba8x4.map { $0 << 8 | $0 >> 0 }
 }
 
-public func rgba_12to8(_ rgba12121212 : [UInt16]) -> [UInt8] {
-    return rgba12121212.map { UInt8($0 >> 4) }
+//----------------------------------------------------------------------------//
+//                             10-bit source                                  //
+//----------------------------------------------------------------------------//
+
+/* Down-sample */
+@inlinable
+public func rgba_10to8(_ rgba10x4 : [UInt16]) -> [UInt16] {
+    return rgba10x4.map { $0 >> 2 }
 }
 
-public func rgba_12to10(_ rgba12121212 : [UInt16]) -> [UInt16] {
-    return rgba12121212.map { $0 >> 2 }
+@inlinable
+public func rgba_10to12(_ rgba10x4 : [UInt16]) -> [UInt16] {
+    return rgba10x4.map { $0 << 2 | $0 >> 8 }
+}
+
+@inlinable
+public func rgba_10to16(_ rgba10x4 : [UInt16]) -> [UInt16] {
+    return rgba10x4.map { $0 << 6 | $0 >> 4 }
+}
+
+//----------------------------------------------------------------------------//
+//                             12-bit source                                  //
+//----------------------------------------------------------------------------//
+
+/* Down-sample */
+@inlinable
+public func rgba_12to8(_ rgba12x4 : [UInt16]) -> [UInt16] {
+    return rgba12x4.map { $0 >> 4 }
+}
+
+/* Down-sample */
+@inlinable
+public func rgba_12to10(_ rgba12x4 : [UInt16]) -> [UInt16] {
+    return rgba12x4.map { $0 >> 2 }
+}
+
+@inlinable
+public func rgba_12to16(_ rgba12x4 : [UInt16]) -> [UInt16] {
+    return rgba12x4.map { $0 << 4 | $0 >> 8 }
+}
+
+//----------------------------------------------------------------------------//
+//                             16-bit source                                  //
+//----------------------------------------------------------------------------//
+
+/* Down-sample */
+@inlinable
+public func rgba_16to8(_ rgba16x4 : [UInt16]) -> [UInt16] {
+    return rgba16x4.map { $0 >> 8 }
+}
+
+/* Down-sample */
+@inlinable
+public func rgba_16to10(_ rgba16x4 : [UInt16]) -> [UInt16] {
+    return rgba16x4.map { $0 >> 6 }
+}
+
+/* Down-sample */
+@inlinable
+public func rgba_16to12(_ rgba16x4 : [UInt16]) -> [UInt16] {
+    return rgba16x4.map { $0 >> 4 }
 }
--- Tests/ImageIntermediaTests/RGBATests.swift
+++ Tests/ImageIntermediaTests/RGBATests.swift
@@ -10,9 +10,56 @@ import XCTest
 
 final class RGBATests : XCTestCase {
     func test_8to10() {
+        XCTAssertEqual(rgba_8to10([0, 1, 255]), [0, 4, 1023])
+        /* lossless conversion */
         XCTAssertEqual(
-          rgba_8to10(Array<UInt8>(0 ... 255)),
-          Array<UInt16>(stride(from: 1, through: 1024, by: 4))
+          rgba_10to8(rgba_8to10(Array(0 ... 255))),
+          Array(0 ... 255)
+        )
+    }
+
+    func test_8to12() {
+        XCTAssertEqual(rgba_8to12([0, 1, 255]), [0, 16, 4095])
+        /* lossless conversion */
+        XCTAssertEqual(
+          rgba_12to8(rgba_8to12(Array(0 ... 255))),
+          Array(0 ... 255)
+        )
+    }
+
+    func test_8to16() {
+        XCTAssertEqual(rgba_8to16([0, 1, 255]), [0, 257, 65535])
+        /* lossless conversion */
+        XCTAssertEqual(
+          rgba_16to8(rgba_8to16(Array(0 ... 255))),
+          Array(0 ... 255)
+        )
+    }
+
+    func test_10to12() {
+        XCTAssertEqual(rgba_10to12([0, 1, 1023]), [0, 4, 4095])
+        /* lossless conversion */
+        XCTAssertEqual(
+          rgba_12to10(rgba_10to12(Array(0 ... 1023))),
+          Array(0 ... 1023)
+        )
+    }
+
+    func test_10to16() {
+        XCTAssertEqual(rgba_10to16([0, 1, 1023]), [0, 64, 65535])
+        /* lossless conversion */
+        XCTAssertEqual(
+          rgba_16to10(rgba_10to16(Array(0 ... 1023))),
+          Array(0 ... 1023)
+        )
+    }
+
+    func test_12to16() {
+        XCTAssertEqual(rgba_12to16([0, 1, 4095]), [0, 16, 65535])
+        /* lossless conversion */
+        XCTAssertEqual(
+          rgba_16to12(rgba_12to16(Array(0 ... 4095))),
+          Array(0 ... 4095)
         )
     }
 }
